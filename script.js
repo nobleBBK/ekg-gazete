1b672ad3de4dad125330dbbba7fabe0d4a4a20de
nobleBBK <bugraberkcankaya@gmail.com>
2026-04-28 15:27:22 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,12 @@
+// EKG - Script Dosyası Çalışıyor mu Kontrolü
+console.log("EKG Projesi: Bağlantı Başarılı. Üstat, site yayına hazır!");
+
+// İleride slider'ı otomatik kaydırmak istersen burayı kullanacağız
+function sliderBaslat() {
+    console.log("Slider fonksiyonu hazır bekliyor...");
+}
+
+// Sayfa tamamen yüklendiğinde çalışacak komutlar
+document.addEventListener("DOMContentLoaded", () => {
+    sliderBaslat();
+});
