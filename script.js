3fa034b1bdec3425aa4801d7ee931ba5bdbf6224
nobleBBK <bugraberkcankaya@gmail.com>
2026-04-29 22:59:50 +0300

    menu drop degil genisler

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function(e) {
     const dropdown = document.querySelector('.dropdown');
     const btn = document.querySelector('.dropbtn');
 
-    if (e.target === btn) {
+    // Eğer basılan yer Koleksiyonlar butonuysa
+    if (e.target === btn || btn.contains(e.target)) {
         e.preventDefault();
         e.stopPropagation();
         dropdown.classList.toggle('is-open');
-    } else {
-        if (dropdown) dropdown.classList.remove('is-open');
+    } 
+    // Başka bir yere basılırsa kapat
+    else {
+        if (dropdown) {
+            dropdown.classList.remove('is-open');
+        }
     }
 });
